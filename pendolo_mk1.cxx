27383646b5415ba9a0a543906276d16d11b9562d
albertos-2002 <93335022+albertos-2002@users.noreply.github.com>
2021-10-28 19:32:15 +0200

    Update pendolo_mk1.cxx

--- pendolo_mk1.cxx
+++ pendolo_mk1.cxx
@@ -1,33 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int numero_misure_while = 0;
-int munero_misure_fatte = 0;
-double float somma_misure = 0; 
-double float valore_misura = 0;
-double float media_aritmetica = 0;
-
 int main () {
+
+    int numero_misure_while = 0;
+    int numero_misure_fatte = 0;
+    float somma_misure = 0.0; 
+    float valore_misura = 0.0;
+    float media_aritmetica = 0.0;
     
     cout << "Inserire il numero totale delle misure effettuate" << endl;
-    cin >> numero_misure_fatte; 
+    cin >> numero_misure_fatte;
+    cout << endl; 
     
-    while (numero_misure_while <= numero_misure_fatte) {
+    while (numero_misure_while < numero_misure_fatte) {
         
         cout << "Inserire il valore numerico della misura" << endl;
         cin >> valore_misura;
+        cout << endl;
         
         somma_misure = somma_misure + valore_misura;
 
-        cout << "Al momento la somma delle varie misure vale" << endl;
+        cout << "Al momento la somma delle varie misure vale\t" << somma_misure << endl << endl;
+        
         
         numero_misure_while = numero_misure_while + 1;
 
     }
+
+// calcolo della media aritmetica    
+    media_aritmetica = somma_misure / numero_misure_fatte;
     
-media_aritmetica = somma_misure / numero_misure_fatte;
-cout << "La media aritmetica delle " << numero_misure_fatte << "misure eseguite è" << endl;
-cout << media_aritmetica << endl;
+//presentazione in stdout dei dati relativi alla media aritmetica    
+    cout << "La media aritmetica delle " << numero_misure_fatte << " misure eseguite è" << endl << endl;
+    cout << media_aritmetica << endl << endl;
 
     return 0;
 }
