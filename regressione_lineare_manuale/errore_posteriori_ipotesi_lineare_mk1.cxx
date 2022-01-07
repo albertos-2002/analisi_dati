7052bb68bdb93cbcd67ade759516a3750432b221
albertos-2002 <93335022+albertos-2002@users.noreply.github.com>
2022-01-07 21:38:12 +0100

    Add files via upload

---
+++ regressione_lineare_manuale/errore_posteriori_ipotesi_lineare_mk1.cxx
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+
+long double errore_posteriori = 0;
+
+//definizione dei vettori delle misure;
+vector <float> xi;
+vector <float> yi;
+
+//definizione variabili calcolo
+float intercetta = 0;
+float coefficiente_angolare = 0;
+int numerosita_campione = 0;
+
+//definizione variabili input file
+ifstream file_in_yi;
+string file_in_yi_str = "";
+ifstream file_in_xi;
+string file_in_xi_str = "";
+ifstream file_in_intercetta;
+string file_in_intercetta_str = "";
+ifstream file_in_coefficiente_angolare;
+string file_in_coefficiente_angolare_str = "";
+
+string valore_miusra_str = "";
+float valore_miusra = 0;
+
+//cin file lettura
+cout << "Inserire il nome del file da cui leggere xi" << endl;
+cin >> file_in_xi_str;
+cout << "Inserire il nome del file da cui leggere yi" << endl;
+cin >> file_in_yi_str;
+cout << "Inserire il nome del file per il coefficiente angolare (b) della retta interpolante" << endl;
+cin >> file_in_coefficiente_angolare_str;
+cout << "Inserire il nome del file pee l'intercetta (a) della retta interpolante" << endl;
+cin >> file_in_intercetta_str;
+
+
+//lettura da file
+file_in_xi.open(file_in_xi_str);
+  //controllo apertura del file
+    if (file_in_xi.fail()) {
+      cout << "Errore nell'apertura del file relativo a xi" << endl;
+      return 1;
+    }
+
+  while (!file_in_xi.eof()){
+    getline (file_in_xi, valore_miusra_str, ',');
+    valore_miusra = stod(valore_miusra_str);
+    xi.push_back(valore_miusra);
+  }
+file_in_xi.close();
+
+//pulizia delle variabili
+  valore_miusra_str = "";
+  valore_miusra = 0;
+
+file_in_yi.open(file_in_yi_str);
+  //controllo apertura del file
+  if (file_in_yi.fail()) {
+    cout << "Errore nel apertura del file relativo a yi" << endl;
+    return 1;
+  }
+
+  while (!file_in_yi.eof()){
+    getline (file_in_yi, valore_miusra_str, ',');
+    valore_miusra = stod(valore_miusra_str);
+    yi.push_back(valore_miusra);
+  }
+
+file_in_yi.close();
+
+
+//controllo uguaglianza dimensione vettori
+if (xi.size() != yi.size()){
+  cout << "I vettori xi e yi non hanno la stessa dimensione" << endl;
+  return 2;
+}
+
+numerosita_campione = xi.size();
+
+//pulizia delle variabili
+  valore_miusra = 0;
+  valore_miusra_str = "";
+file_in_intercetta.open(file_in_intercetta_str);
+  //controllo paertura del file
+  if (file_in_intercetta.fail()){
+    cout << "Errore nell'apertura del file relativo alla pendenza (a) " << endl;
+    return 1;
+  }
+
+  while (!file_in_intercetta.eof()){
+    getline(file_in_intercetta, valore_miusra_str);
+    valore_miusra = stod(valore_miusra_str);
+  }
+  intercetta = valore_miusra;
+file_in_intercetta.close();
+
+//pulizia delle variabili
+  valore_miusra = 0;
+  valore_miusra_str = "";
+file_in_coefficiente_angolare.open(file_in_coefficiente_angolare_str);
+  //controllo apertura del file
+  if (file_in_coefficiente_angolare.fail()){
+    cout << "Errore nell'apertura del file relativo al coerfficiente angolare (b)" << endl;
+    return 1;
+  }
+
+  while (!file_in_coefficiente_angolare.eof()){
+    getline(file_in_coefficiente_angolare, valore_miusra_str);
+    valore_miusra = stod(valore_miusra_str);
+  }
+  coefficiente_angolare = valore_miusra;
+file_in_coefficiente_angolare.close();
+
+
+long double sommatoria = 0;
+//calcolo sommatoria
+long double parametro_c = 0;
+long double parametro_d = 0;
+long double parametro_e = 0;
+
+for (int i=0; i<numerosita_campione; ++i){
+  parametro_c = (coefficiente_angolare * xi.at(i)) + intercetta;
+  parametro_d = yi.at(i) - parametro_c;
+  parametro_e = pow(parametro_d,2);
+  sommatoria = sommatoria + parametro_e;
+}
+
+
+//calcolo dell'errore a posteriori
+float parametro_f = 0;
+float parametro_g = 0;
+float parametro_h = 0;
+
+parametro_f = numerosita_campione - 2;
+parametro_g = 1 / parametro_f;
+parametro_h = parametro_g * sommatoria;
+
+errore_posteriori = sqrt(parametro_h);
+
+cout << "Il valore dell'errore a posteriori" << endl;
+cout << errore_posteriori << endl;
+
+
+//scrittura su file di output
+ofstream file_out;
+string file_out_str;
+
+cout << "Inserire il nome del file di output per l'errore a posteriori" << endl;
+cin >> file_out_str;
+
+file_out.open(file_out_str);
+  //controllo apertura del file
+  if (file_out.fail()){
+    cout << "Errore nell'apertura del file di output" << endl;
+    return 1;
+  }
+
+  file_out << errore_posteriori;
+
+file_out.close();
+
+return 0;
+}
