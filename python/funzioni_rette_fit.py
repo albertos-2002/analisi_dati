f655f9a33d500ee62ca8831c4f65390a1d3defa0
Alberto <93335022+albertos-2002@users.noreply.github.com>
2023-11-24 13:40:45 +0100

    Update and rename retta_fit_xba.py to funzioni_rette_fit.py

--- python/funzioni_rette_fit.py
+++ python/funzioni_rette_fit.py
@@ -5,3 +5,11 @@ def retta_fit_xba(x, b,a):
     dummy = (x[i]*b) + a
     y.append(dummy)
   return y
+
+def retta_fit_xa(x, a):
+  y = []
+  for i in range(len(x)):
+    dummy = 0
+    dummy = (x[i]*0) + a
+    y.append(dummy)
+  return y
